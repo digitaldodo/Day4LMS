cfe1804128f816fcb805c5b6d97c533e86f43a08
Ansh <anshkmrn@gmail.com>
2026-01-20 12:38:21 +0530

    Initial commit - Day4LMS project

---
+++ src/main/java/com/example/day4lms/Day4LmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.day4lms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day4LmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Day4LmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/day4lms/Day4LmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.day4lms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day4LmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
